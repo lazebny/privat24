ef00548540b9661f770bed518b57ca9d1de959bb
Vadim Lazebny <vadim.lazebny@gmail.com>
2014-09-01 16:34:28 +0300

    Fix

--- lib/privat24/railtie.rb
+++ lib/privat24/railtie.rb
@@ -2,8 +2,8 @@ require 'privat24/privat24_helper'
 
 module Privat24
   class Railtie < Rails::Railtie
-    initializer 'private24.view_helpers' do |app|
-      ActionView::Base.send :include, Private24::Private24Helper
+    initializer 'privat24.view_helpers' do |app|
+      ActionView::Base.send :include, Privat24::Privat24Helper
     end
   end
 end
